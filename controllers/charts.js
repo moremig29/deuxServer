331f404507b6cdd1fdb3b9b182b8f9d736a20513
moremig29 <miguelangel.moreira@gmail.com>
2023-08-06 11:02:41 -0400

    avance chart

--- controllers/charts.js
+++ controllers/charts.js
@@ -4,18 +4,57 @@ const Gasto = require('../models/Gasto');
 // registrar compra
 const gastosTotales = async ( req, resp = response ) => {
 
-
   const uid = req.uid;
 
   try {
 
     let dbGastos = await Gasto.find();
 
+    let data = {
+      ingreso: 0,
+      alimentacion: 0,
+      transporte: 0,
+      salud: 0,
+      educacion: 0,
+      deportes: 0,
+      compras: 0,
+      otros: 0
+    }
+    
+    for (const gasto of dbGastos) {
+      
+      if ( gasto.type === 8 ) {
+        data.ingreso += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 1 ) {
+        data.alimentacion += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 2 ) {
+        data.transporte += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 3 ) {
+        data.salud += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 4 ) {
+        data.educacion += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 5 ) {
+        data.deportes += Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 6 ) {
+        data.compras +=  Math.round( gasto.ammount );
+      }
+      if ( gasto.type === 7 ) {
+        data.otros +=  Math.round( gasto.ammount );
+      }
+    }
+
     // generar response
     return resp.status(201).json({
       ok: true,
       msg: 'ok',
       uid,
+      data,
       dbGastos
     });
     
