48c8749532f6f09d506a1f32372ebbdf5cb6c9ff
moremig29 <miguelangel.moreira@gmail.com>
2023-09-17 19:34:06 -0400

    editar y eliminar insumos

--- controllers/insumo.js
+++ controllers/insumo.js
@@ -56,9 +56,62 @@ const getInsumo = async ( req, resp = response ) => {
     });
 
   }
-} 
+}
+
+// editar insumo
+const putInsumo = async ( req, resp = response ) => {
+
+  const id = req.params.id;
+  const insumo = req.body;
+  const dbInsumo = await Insumo.findByIdAndUpdate( id, insumo, { new: true } );
+
+  try {
+
+    // generar response
+    return resp.status(201).json({
+      ok: true,
+      msg: 'Insumo editado',
+      insumo: dbInsumo
+    });
+    
+  } catch (error) {
+
+    return resp.status(500).json({
+      ok: false,
+      msg: 'No se puede editar'
+    });
+
+  }
+}
+
+// delete insumo
+const deleteInsumo = async ( req, resp = response ) => {
+
+  const id = req.params.id;
+  const dbInsumo = await Insumo.findByIdAndDelete( id );
+
+  try {
+
+    // generar response
+    return resp.status(201).json({
+      ok: true,
+      msg: 'Insumo Eliminado',
+      dbCompra: dbInsumo
+    });
+    
+  } catch (error) {
+
+    return resp.status(500).json({
+      ok: false,
+      msg: 'No se puede mostrar'
+    });
+
+  }
+}
 
 module.exports = {
   crear,
-  getInsumo
+  getInsumo,
+  putInsumo,
+  deleteInsumo
 }
--- routes/insumo.js
+++ routes/insumo.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { getInsumo, crear } = require('../controllers/insumo');
+const { getInsumo, crear, putInsumo, deleteInsumo } = require('../controllers/insumo');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get( '/', validarJWT, getInsumo );
 router.post( '/', validarJWT, crear );
+router.put('/:id', validarJWT, putInsumo );
+router.delete('/:id', validarJWT, deleteInsumo );
 
 module.exports = router;
